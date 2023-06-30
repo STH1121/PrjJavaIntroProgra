3abdab84a7ccafe9c7bc4252b4aabea086b484aa
STH1121 <sth11214127@gmail.com>
2023-06-30 17:56:19 -0600

    Menu basico

--- PRJIntroPrograGrupo6/src/main/java/prjsemana03/prjintroprogragrupo6/PRJIntroPrograGrupo6.java
+++ PRJIntroPrograGrupo6/src/main/java/prjsemana03/prjintroprogragrupo6/PRJIntroPrograGrupo6.java
@@ -3,7 +3,7 @@
  */
 
 package prjsemana03.prjintroprogragrupo6;
-
+import javax.swing.JOptionPane;
 /**
  *
  * @author Steven
@@ -12,5 +12,56 @@ public class PRJIntroPrograGrupo6 {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        char usuario = ' ';
+        
+        do {
+            usuario = JOptionPane.showInputDialog("Seleccione una opción:"
+                    + "\nA. Modulo Ventas"
+                    + "\nB. Modulo Compras"
+                    + "\nC. Modulo usuarios"
+                    + "\nD. Modulo Inventario"
+                    + "\nE. Modulo Administracion"
+                    + "\nF. Seguridad del sistema"
+                    + "\nS. Salir").toUpperCase().charAt(0);
+            
+        switch (usuario) {
+                case 'A':
+                    JOptionPane.showMessageDialog(null, "Iniciando modulo de ventas");
+                    // this.ventas();
+                    break;
+                case 'B':
+                    JOptionPane.showMessageDialog(null, "Iniciando modulo de compras");
+                    // this.compras();
+                    break;
+                case 'C':
+                    JOptionPane.showMessageDialog(null, "Iniciando modulo usuarios");
+                    // this.usuarios();
+                    break;
+                case 'D':
+                    JOptionPane.showMessageDialog(null, "Iniciando modulo inventario");
+                    // this.inventario();
+                    break;
+                case 'E':
+                    JOptionPane.showMessageDialog(null, "Iniciando modulo administracion");
+                    // this.administracion();
+                    break;    
+                case 'F':
+                    JOptionPane.showMessageDialog(null, "Abriendo seguridad del sistema");
+                    // this.seguridad();
+                    break;    
+                
+                case 'S':
+                    JOptionPane.showMessageDialog(null, "Apagando aplicacion.");
+                     
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "Digite una opcion valida");
+                    break;
+            }
+            
+            
+            
+            
+        } while (usuario != 'S');
     }
 }
